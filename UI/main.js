3e19412bda4b0c2fbb7003fdb61439775a86b269
TheSteelGuy <collotuos0@gmail.com>
2018-08-10 12:26:07 +0300

    host Ui on githubpages

---
+++ UI/main.js
@@ -0,0 +1,14 @@
+
+const  displayCommentForm = () =>{
+	document.getElementById("commentBox").style.display = 'block';
+}
+
+
+const  displayQuestionForm = () =>{
+	document.getElementById("quizBody").style.display = 'block';
+	document.getElementById("quizForm").style.display = 'block';
+}
+const  hideQuestionForm = () =>{
+	document.getElementById("quizBody").style.display = 'none';
+	document.getElementById("quizForm").style.display = 'none';
+}
